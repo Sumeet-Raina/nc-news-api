3b4e3963693c12e0ff32c8ee327f2474f60e69c4
Sumeet Raina <sumeet@Mac.broadband>
2025-04-28 13:08:21 +0100

    Setup complete

--- northcoders-news-BE/db/connection.js
+++ northcoders-news-BE/db/connection.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || "development";
-console.log(__dirname, "<=====");
+
 require("dotenv").config({ path: `${__dirname}/.env.${ENV}` });
 
 const db = new Pool();
